e3a42493401b368243cdcedec54786729167099f
曾鸿发 <zenghongfa@sundear.com>
2022-01-06 23:38:33 +0800

    leetcode25

--- src/main/java/leetcode25/Main.java
+++ src/main/java/leetcode25/Main.java
@@ -3,7 +3,23 @@ package leetcode25;
 public class Main {
 
     public static void main(String[] args) {
-
+        ListNode head = new ListNode(1);
+        ListNode head1 = new ListNode(2);
+        ListNode head2 = new ListNode(3);
+        ListNode head3 = new ListNode(4);
+        ListNode head4 = new ListNode(5);
+        head.next = head1;
+        head1.next = head2;
+        head2.next = head3;
+        head3.next = head4;
+        Solution solution = new Solution();
+        ListNode listNode = solution.reverseKGroup(head, 2);
+        ListNode tmp = listNode;
+        while (tmp != null){
+            System.out.print(tmp.val + " ");
+            tmp = tmp.next;
+        }
+        System.out.println();
     }
 }
 
@@ -26,7 +42,25 @@ class ListNode {
 
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        return null;
+        ListNode start = head;
+        ListNode end = getKGroupEnd(start, k);
+        if(end == null){
+            return head;
+        }
+        head = end;
+        reverse(start, end);
+        ListNode lastEnd = start;
+        while (lastEnd.next != null){
+            start = lastEnd.next;
+            end = getKGroupEnd(lastEnd.next, k);
+            if(end == null){
+                return head;
+            }
+            reverse(start, end);
+            lastEnd.next = end;
+            lastEnd = start;
+        }
+        return head;
     }
 
     public static ListNode getKGroupEnd(ListNode start, int k) {
@@ -34,5 +68,19 @@ class Solution {
             start = start.next;
         }
         return start;
+    }
+
+    public static void reverse(ListNode start, ListNode end){
+        end = end.next;
+        ListNode pre = null;
+        ListNode cur = start;
+        ListNode next = null;
+        while (cur != end){
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        start.next = end;
     }
 }
