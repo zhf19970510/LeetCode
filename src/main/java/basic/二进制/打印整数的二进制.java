f1f09dfe3b56bfd17ddc88dee4a3a8af1f09dce3
曾鸿发 <44803247+zhf19970510@users.noreply.github.com>
2023-09-03 09:04:48 +0800

    打印整数的二进制

---
+++ src/main/java/basic/二进制/打印整数的二进制.java
@@ -0,0 +1,34 @@
+package basic.二进制;
+
+public class 打印整数的二进制 {
+
+    public static void main(String[] args) {
+        int num = 4;
+        print(num);
+        System.out.println();
+        // 整数最小数的相反数
+
+        int a = Integer.MIN_VALUE;
+        int b = ~a;
+        int c = ~a + 1;
+        int d = -a;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(a == b);
+        System.out.println(a == c);
+        System.out.println(a == d);
+    }
+
+    /**
+     * 打印整数的32进制数据
+     *
+     * @param num
+     */
+    public static void print(int num) {
+        for (int i = 31; i >= 0; i--) {
+            System.out.print((num & (1 << i)) == 0 ? "0" : "1");
+        }
+    }
+}
